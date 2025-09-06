053870e9e08611eefb7213074fdb25721fc4a4bf
Rafiza-Fried-Chicken <mangvios0@gmail.com>
2025-09-06 20:26:03 +0700

    Update main.js

--- assets/CSS/js/main.js
+++ assets/CSS/js/main.js
@@ -1,1 +1,9 @@
+// main.js - script umum
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Netshosting loaded");
 
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    if(user) {
+        document.getElementById('user-display')?.innerText = user.username;
+    }
+});
